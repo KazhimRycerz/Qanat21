6e12045b1926a970cf10d17bea570225de9254c9
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-11-21 22:30:42 +0100

    septup finished

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import "./Footer.scss";
+import { NavLink, Link } from "react-router-dom";
+
+const Footer = () => {
+
+  return (
+    
+      <div className="footer">
+          <p id="footer_logo">Q</p>  
+        <ul>
+          <li>NN</li>
+          <li><p>
+         Qanat21 uG in Gründung <br />
+         Marienfelder Str. 18<br />
+         33330 Gütersloh</p></li>
+          <li>NN</li>
+          <li>NN</li>
+        </ul>
+      </div>
+    
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,26 @@ import './Home.scss'
 import Qanat_Logo from '../Images/Qanat21_Logo_Q.png'
 import Qanat_Logo_Schriftzug from '../Images/Qanat_Schriftzug.png'
 import Header from '../components/Header/Header.js'
+import Footer from '../components/Footer/Footer.js'
 
 
 const Home = () => (
     <div className="bg">
-      <Header />
+     { <Header />}
       {/* <h1>Hier entsteht die Qanat21 Webseite</h1> */}
-        <div id="secondoption">
-          <p id="logolang2">qanat<em>21</em></p> 
-          <p id="logoschrift2">Q</p> 
-        </div> 
-        <div id="firstoption">
-          <p id="logolang">qanat<em>21</em></p>
-          <p id="logoschrift">Q</p> 
+        <div id="bg1">
+          <div id="firstoption">
+            <p id="logolang2">qanat<em>21</em></p> 
+            <p id="logoschrift2">Q</p> 
+          </div> 
         </div>
+        <div id="bg2">
+          <div id="secondoption">
+            <p id="logolang">qanat<em>21</em></p>
+            <p id="logoschrift">Q</p> 
+          </div>
+        </div>
+        <Footer />
       {/* <img id="QanatSchriftzug" src={Qanat_Logo_Schriftzug} alt="" />
       <img id="QuanatLogo" src={Qanat_Logo} alt="" /> */}
       

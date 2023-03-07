08ce58d7d49864cbe40108b1ad8fa7be2543b5e8
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2023-03-07 16:36:30 +0100

    mediaqueries

--- src/pages/TheGoal.jsx
+++ src/pages/TheGoal.jsx
@@ -10,7 +10,16 @@ const Home = () => (
      <Header />
         <div className="bg1">
           <div className="firstoption" id="goal">
+            {/* <div> */}
             <h2>Die unternehmerische Aufgabe</h2>
+            {/* <ul>
+              <li>Datenerhebung und Wetterprognosen</li>
+              <li> Planungsansätze aus bestehenden Projekten</li>
+              <li> Konkrete Lösung und erstes Projekt </li>
+              <li> Umsetzung eines Referenzprojektes </li>
+              <li> Wissenschaftlicher Nachweis der Funktionalität </li>
+            </ul> */}
+            {/* </div> */}
             <img id="goalimg" src={projektziel} alt="" />
           </div> 
         </div>
